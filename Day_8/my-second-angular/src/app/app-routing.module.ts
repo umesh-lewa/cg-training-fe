f273d4c1fa0260531657bf3d8df2de30ed27bf17
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-06-08 16:15:04 +0530

    home routing

--- Day_8/my-second-angular/src/app/app-routing.module.ts
+++ Day_8/my-second-angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { BasketComponent } from './basket/basket.component';
 import { ContactbookComponent } from './contactbook/contactbook.component';
 import { TodoAppComponent } from './todo-app/todo-app.component';
 
 const routes: Routes = [
+ { path:"home",component: AppComponent},
  { path:"todo",component: TodoAppComponent},
  { path:"contact",component: ContactbookComponent}
 ];
